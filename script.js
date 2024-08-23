3b7295ed5421b98dcf0c92960757c80871f42f3f
Coder219 <gbdudenyc@gmail.com>
2024-08-23 18:05:14 -0400

    added brush color and clear button

--- script.js
+++ script.js
@@ -1,38 +1,50 @@
 let gridExists = false;
+let paintColor = 'black';
+let canvasColor = 'white';
+let cursorColor = 'darkgrey';
+const inputElement = document.querySelector('ul');
 const rowInput = document.querySelector('.row-input');
 const columnInput = document.querySelector('.column-input');
 const loadGridButton = document.querySelector('.load-button');
-
+const clearGridButton = document.querySelector('.erase-button');
+const colorButtons = document.querySelectorAll('.colors button');
+function createBox(row,column,rowContainer){
+    const box = document.createElement("div");
+    box.className += ` row${row} column${column} box`;
+    box.style.backgroundColor = canvasColor;
+    rowContainer.appendChild(box);
+    box.addEventListener('mouseover', (event) => {
+    if (!box.classList.contains('painted')) box.style.backgroundColor = cursorColor;
+    if (event.buttons === 1){
+        box.style.backgroundColor = paintColor;
+        box.classList.add('painted');
+        };
+    });
+    box.addEventListener('mouseleave', function(){if (!box.classList.contains('painted')) box.style.backgroundColor = canvasColor;});
+}
 function createGrid(width,height){
     gridExists = true;
     const gridContainer = document.createElement("div");
-    gridContainer.classList.add(`grid`);
-    gridContainer.classList.add('container');
+    gridContainer.className += 'grid container';
     document.body.appendChild(gridContainer);
     for (let row=1; row<=height; row++){
         rowContainer = document.createElement("div");
         gridContainer.appendChild(rowContainer);
-        rowContainer.classList.add('row');
-        rowContainer.classList.add('container');
-        for(let column=1; column<=width; column ++){
-            const box = document.createElement("div");
-            box.classList.add(`row${row}`);
-            box.classList.add(`column${column}`);
-            box.classList.add('box');
-            box.addEventListener('mouseover', function(){
-                console.log(`Mouse over row${row} column${column}`);
-            });
-            box.style.padding = (screen.height)/height
-            rowContainer.appendChild(box);
-        }
-    }
-
+        rowContainer.className += ' row container';
+        for(let column=1; column<=width; column ++) createBox(row,column,rowContainer);    
+    };
 }
-
-
-
+function clearGrid(){
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach((box) => {
+        box.classList.remove('painted');
+        box.style.backgroundColor = canvasColor;
+    });
+}
 loadGridButton.addEventListener('click', function() {
     if (!gridExists) createGrid(rowInput.value,columnInput.value);
+    loadGridButton.remove();
+    inputElement.remove();
 });
-
-    
+clearGridButton.addEventListener('click', function() {clearGrid()});
+colorButtons.forEach(function(button){button.addEventListener('click', function(event){paintColor = button.id});});
