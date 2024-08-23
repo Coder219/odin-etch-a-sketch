fd6aaf872be137cab3547428af1c567614de623c
Coder219 <gbdudenyc@gmail.com>
2024-08-23 12:54:06 -0400

    Created basic grid and hover sys

---
+++ script.js
@@ -0,0 +1,42 @@
+let gridExists = false;
+const rowInput = document.querySelector('.row-input');
+const columnInput = document.querySelector('.column-input');
+const loadGridButton = document.querySelector('.load-button');
+
+function createGrid(width,height){
+    gridExists = true;
+    const gridContainer = document.createElement("div");
+    gridContainer.classList.add(`grid`);
+    gridContainer.classList.add('container');
+    document.body.appendChild(gridContainer);
+    for (let row=1; row<=height; row++){
+        rowContainer = document.createElement("div");
+        gridContainer.appendChild(rowContainer);
+        rowContainer.classList.add('row');
+        rowContainer.classList.add('container');
+        for(let column=1; column<=width; column ++){
+            const box = document.createElement("div");
+            box.classList.add(`row${row}`);
+            box.classList.add(`column${column}`);
+            box.classList.add('box');
+            box.addEventListener('mouseover', function(){
+                console.log(`Mouse over row${row} column${column}`);
+                box.style.backgroundColor = 'red';
+            });
+            box.addEventListener('mouseleave', function(){
+                box.style.backgroundColor = 'darkgrey';
+            });
+            box.textContent = `(${row},${column})`
+            rowContainer.appendChild(box);
+        }
+    }
+
+}
+
+
+
+loadGridButton.addEventListener('click', function() {
+    if (!gridExists) createGrid(rowInput.value,columnInput.value);
+});
+
+    
